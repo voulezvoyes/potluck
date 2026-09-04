94d33a89a5292b307afd23723c88290e9905bd86
Jaesung Kim <voulezvoyes@gmail.com>
2026-09-03 23:04:17 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="potluck-shell-v1";
+const CACHE="potluck-shell-v2";
 const SHELL=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./apple-touch-icon.png"];
 
 self.addEventListener("install",event=>{
